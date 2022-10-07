f7a52471c923099554a78a55ffef7d799057c5a3
EWanjau <wamwanjau@gmail.com>
2022-10-07 19:18:00 +0300

    removed spaces in header file

--- sort.h
+++ sort.h
@@ -16,9 +16,10 @@ void print_list(const listint_t *list);
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 #endif
+
