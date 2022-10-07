285a461ec5dca9b913b716644cb6983472c34f73
EWanjau <wamwanjau@gmail.com>
2022-10-07 21:11:34 +0300

    corrected the error of looping more

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,22 +4,23 @@
  * bubble_sort - sorts the array input in ascending order
  * @array: the array to be sorted
  * @size: the size of array
- * Return: the new sorted array
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j;
+	int temp;
 
 	if (size < 2)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - i; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				int temp = array[j];
+				temp = array[j];
 
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
