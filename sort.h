90458cfef4b37a58c91e9ca28f8388c8a455b217
EWanjau <wamwanjau@gmail.com>
2022-10-13 15:52:34 +0300

    updated the prototype of shell sort

--- sort.h
+++ sort.h
@@ -27,5 +27,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swap(int index1, int index2);
+void quick_recursion(int *array, int left, int right, size_t size);
+int partition(int *array, int left, int right, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
